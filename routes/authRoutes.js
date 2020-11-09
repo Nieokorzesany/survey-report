51510688895b32b290cc14a17ccc6df6067a2e58
Piotr Wlazeł <pancu1222@gmail.com>
2020-11-09 15:01:46 +0100

    upd

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,18 @@ module.exports = (app) => {
     })
   );
 
+  app.get("/", (req, res) => {
+    res.send("dziala");
+  });
+
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/test/current_user", (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 };
